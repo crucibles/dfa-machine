eb4f46052fb7a5c95061df9e8179b15f0dd7bc5d
crucibles <alvaro_cedric@yahoo.com>
2018-02-20 13:53:31 +0800

    added DFATable

--- src/DFAMachine.java
+++ src/DFAMachine.java
@@ -32,6 +32,48 @@ public class DFAMachine {
 		initialize();
 	}
 
+	/*
+	* test
+	*/
+	private void createTable() {
+
+		String transitions = "-,A,B,A\n$,B,B,C\n+,C,B,A";
+		String inp = "110011\n0111110001\n1001010\n100";
+
+		if (checker(transitions)) {
+			String[] lines = transitions.trim().split("\\s");
+
+			for (int x = 0; x < lines.length; x++) {
+
+				String line = lines[x];
+				System.out.println(line);
+				String[] tokens = line.trim().split(",");
+
+				dState.getVector().add(new DFAState(tokens[0], tokens[1], tokens[2], tokens[3]));
+
+			}
+		}
+
+		System.out.println(dState.getVector().get(0).getStateName());
+		System.out.println(dState.getVector().get(1).getStateName());
+		System.out.println(dState.getVector().get(2).getStateName());
+	}
+
+
+	private boolean checker(String transitions) {
+		if (transitions.indexOf('-') != transitions.lastIndexOf('-')) {
+			System.out.println("more than 1 start state!");
+			return false;
+		}
+
+		if (transitions.indexOf('+') == transitions.lastIndexOf('-')) {
+			System.out.println("has a state that is both start and final!");
+			return false;
+		}
+
+		return true;
+	}
+
 	/**
 	 * Sets up the program's GUI.
 	 */
